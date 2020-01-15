7f64054db8a76948912b5d61140ba27289a68635
gurszky.tamas <gurszky.tamas@danubiusinfo.hu>
2020-01-15 23:40:43 +0100

    move selector to item level

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { map, curry } from "ramda";
-import produce from "immer";
+import { map, prop } from "ramda";
 import { createSelector } from "reselect";
 import { connect } from "react-redux";
 
@@ -8,30 +7,22 @@ import List from "@material-ui/core/List";
 
 import SidebarItem from "./SidebarItem";
 
-const extendLearningItem = curry((activeId, learningItem) =>
-  produce(learningItem, draft => {
-    draft.isActive = draft.id === activeId;
-  })
-);
-
 const getLearningItems = state => state.learningItems;
-const getActiveId = state => state.activeId;
 
-const learningItemsSelector = createSelector(
-  [getLearningItems, getActiveId],
-  (learningItems, activeId) => map(extendLearningItem(activeId), learningItems)
+const learningItemIdsSelector = createSelector([getLearningItems], learningItems =>
+  map(prop("id"), learningItems)
 );
 
 const mapStateToProps = state => ({
-  learningItems: learningItemsSelector(state)
+  learningItemIds: learningItemIdsSelector(state)
 });
 
-export const Sidebar = ({ learningItems, setActive }) => {
+export const Sidebar = ({ learningItemIds, setActive }) => {
   console.log("Sidebar render");
   return (
     <List>
-      {learningItems.map(li => (
-        <SidebarItem learningItem={li} key={li.id} />
+      {learningItemIds.map(id => (
+        <SidebarItem learningItemId={id} key={id} />
       ))}
     </List>
   );
--- src/SidebarItem.jsx
+++ src/SidebarItem.jsx
@@ -1,17 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createSelector } from "reselect";
+import { find, curry } from "ramda";
+import produce from "immer";
 
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { connect } from "react-redux";
+
+const extendLearningItem = curry((activeId, learningItem) =>
+  produce(learningItem, draft => {
+    draft.isActive = draft.id === activeId;
+  })
+);
+
+const getLearningItem = (state, props) =>
+  find(li => li.id === props.learningItemId, state.learningItems);
+const getActiveId = state => state.activeId;
+
+const learningItemSelector = createSelector(
+  [getLearningItem, getActiveId],
+  (learningItem, activeId) => extendLearningItem(activeId, learningItem)
+);
+
+const mapStateToProps = (state, props) => ({
+  learningItem: learningItemSelector(state, props)
+});
 
 const mapDispatchToProps = dispatch => ({
   setActive: id => dispatch({ type: "SET_ACTIVE", payload: id })
 });
 
-export const SidebarItem = ({ learningItem, setActive }) => {
+export const SidebarItem = ({ learningItemId, learningItem, setActive }) => {
   console.log("SidebarItem render");
   return (
-    <ListItem button selected={learningItem.isActive} onClick={() => setActive(learningItem.id)}>
+    <ListItem button selected={learningItem.isActive} onClick={() => setActive(learningItemId)}>
       <ListItemText>
         {learningItem.name} - {learningItem.score}
       </ListItemText>
@@ -19,4 +41,4 @@ export const SidebarItem = ({ learningItem, setActive }) => {
   );
 };
 
-export default connect(null, mapDispatchToProps)(SidebarItem);
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarItem);
