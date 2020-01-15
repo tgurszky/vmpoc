b1a7638dda5c1db6771c15713b82b2f36d0203d1
gurszky.tamas <gurszky.tamas@danubiusinfo.hu>
2020-01-15 23:50:25 +0100

    reselect isActive

--- src/SidebarItem.jsx
+++ src/SidebarItem.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createSelector } from "reselect";
-import { find, curry } from "ramda";
-import produce from "immer";
+import { find } from "ramda";
 
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
-const extendLearningItem = curry((activeId, learningItem) =>
-  produce(learningItem, draft => {
-    draft.isActive = draft.id === activeId;
-  })
-);
-
 const getLearningItem = (state, props) =>
   find(li => li.id === props.learningItemId, state.learningItems);
-const getActiveId = state => state.activeId;
+const getIsActive = (state, props) => state.activeId === props.learningItemId;
 
 const makeLearningItemSelector = () =>
-  createSelector([getLearningItem, getActiveId], (learningItem, activeId) =>
-    extendLearningItem(activeId, learningItem)
-  );
+  createSelector([getLearningItem], learningItem => learningItem);
+
+const makeIsActiveSelector = () => createSelector([getIsActive], isActive => isActive);
 
 const makeMapStateToProps = () => {
   const learningItemSelector = makeLearningItemSelector();
+  const isActiveSelector = makeIsActiveSelector();
 
   const mapStateToProps = (state, props) => ({
-    learningItem: learningItemSelector(state, props)
+    learningItem: learningItemSelector(state, props),
+    isActive: isActiveSelector(state, props)
   });
 
   return mapStateToProps;
@@ -36,10 +31,10 @@ const mapDispatchToProps = dispatch => ({
   setActive: id => dispatch({ type: "SET_ACTIVE", payload: id })
 });
 
-export const SidebarItem = ({ learningItemId, learningItem, setActive }) => {
+export const SidebarItem = ({ learningItemId, learningItem, isActive, setActive }) => {
   console.log("SidebarItem render");
   return (
-    <ListItem button selected={learningItem.isActive} onClick={() => setActive(learningItemId)}>
+    <ListItem button selected={isActive} onClick={() => setActive(learningItemId)}>
       <ListItemText>
         {learningItem.name} - {learningItem.score}
       </ListItemText>
